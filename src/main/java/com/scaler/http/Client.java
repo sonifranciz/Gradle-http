f5d87deb57ea43dfb1973b3136b9b27ba1945492
sonia <soniafrancis9899@gmail.com>
2024-08-05 15:25:15 +0100

    okhttp

---
+++ src/main/java/com/scaler/http/Client.java
@@ -0,0 +1,32 @@
+package main.java.com.scaler.http;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import java.io.IOException;
+
+
+public class Client {
+     OkHttpClient okHttpClient;  //declaring the instance variable
+     //for our okhttp class
+
+     public Client(){ //constructor
+           okHttpClient = new OkHttpClient();
+          ///using new keyword
+     }
+
+     public String get(String url){
+
+               Request request = new Request.Builder()
+                       .url(url)
+                       .build();
+               try {
+                    return okHttpClient.newCall(request).execute().body().string();
+               } catch(IOException e){
+                     System.out.println("Error message in get request" + e.getMessage());
+                     e.printStackTrace();
+                    return null;
+          }
+
+     }
+}
+
